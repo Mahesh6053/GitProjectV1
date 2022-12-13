8ad351e73f1fe6289c3a22ec21e2893a29dad5ff
Mahesh6053 <amisagaddamahesh@gmail.com>
2022-12-13 13:07:38 +0530

    ntng

--- RestAssured/src/test/java/RestAssuredRahulShetty/ComplexJsonParse.java
+++ RestAssured/src/test/java/RestAssuredRahulShetty/ComplexJsonParse.java
@@ -7,6 +7,20 @@ import Files.PayLoad;
 import io.restassured.path.json.JsonPath;
 
 public class ComplexJsonParse {
+	
+	/*
+	 * 1. Print No of courses returned by API
+	 * 
+	 * 2.Print Purchase Amount
+	 * 
+	 * 3. Print Title of the first course
+	 * 
+	 * 4. Print All course titles and their respective Prices
+	 * 
+	 * 5. Print no of copies sold by RPA Course
+	 * 
+	 * 6. Verify if Sum of all Course prices matches with Purchase Amount
+	 */
 
 	@Test
 	public void complexParse() {
